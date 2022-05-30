9c822238aa0021fabdf02dac7da964f9d65a7ecd
Bernard <bernardlamptey42@yahoo.com>
2022-05-30 14:05:08 +0200

    changes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class LoginForm extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<Form>
+					<Form.Group className="mb-3" controlId="formBasicEmail">
+						<Form.Label>Username</Form.Label>
+						<Form.Control type="email" placeholder="Enter email" />
+						<Form.Text className="text-muted"></Form.Text>
+					</Form.Group>
+
+					<Form.Group className="mb-3" controlId="formBasicPassword">
+						<Form.Label>Password</Form.Label>
+						<Form.Control type="password" placeholder="Password" />
+					</Form.Group>
+					
+					<Button variant="primary" type="submit">Submit</Button>
+				</Form>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default LoginForm;
